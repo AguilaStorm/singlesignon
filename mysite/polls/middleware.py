146f99ca101e6c558ee33c61a61ad436baea7945
Lucas Tobon <lucas.tobon.v@gmail.com>
2022-07-26 22:22:55 -0500

    Function is_profile_complete was refactored

--- mysite/polls/middleware.py
+++ mysite/polls/middleware.py
@@ -11,27 +11,24 @@ def is_profile_complete(user):
     form = models.ProfileForm.objects.get(site=site)
     form_fields = form.form_fields['fields']
 
-    required_fields = []
     required_choices = {}
     for field in form_fields:
         if field['required']:
             if field.get('choices'):
                 required_choices[field['id']] = field['choices']
-                required_fields.append(field['id'])
             else:
                 required_choices[field['id']] = 'no-choice'
-                required_fields.append(field['id'])
 
     fields_validations = []
     if user.profile.dynamic_fields:
         for user_field, value in user.profile.dynamic_fields.items():
-            if user_field in required_fields and (value in str(required_choices[user_field]) or \
+            if user_field in required_choices.keys() and (value in str(required_choices[user_field]) or \
                 required_choices[user_field] == 'no-choice') and value:
                 fields_validations.append(True)
             else:
                 fields_validations.append(False)
 
-        is_complete = all([*fields_validations, *[field in user.profile.dynamic_fields for field in required_fields]])
+        is_complete = all([*fields_validations, *[field in user.profile.dynamic_fields for field in required_choices.keys()]])
         return is_complete
     else:
         return False
